77589e862f14fa7c1f59b3f34f287f0d79fcbdf0
UtsavGohel <utsavgohel2002@gmail.com>
2022-06-26 19:55:55 +0530

    Project Completed

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CartserviceService } from 'src/app/cartservice.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  public totalItem:number=0;
+  constructor(private cartService:CartserviceService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProduct().subscribe(res=>{
+      this.totalItem = res.length;
+    })
+  }
+
+}
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CartserviceService } from 'src/app/cartservice.service';
+import { ApiserviceService } from 'src/app/service/apiservice.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  public productList:any
+  constructor(private api:ApiserviceService,private cartService : CartserviceService) { }
+
+  ngOnInit(): void {
+    this.api.getProduct().subscribe(res=>{
+      this.productList = res;
+
+      this.productList.forEach((a:any)=>{
+        Object.assign(a,{quantity:1,total:a.price})
+      })
+    })
+  }
+  addtocart(item:any){
+    this.cartService.addtoCart(item);
+  }
+
+}
